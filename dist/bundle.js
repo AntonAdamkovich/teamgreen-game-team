17e41729d69261dea4a8c9c4656c7d96901111ca
AntonAdamkovich <anton.adamkovich@gmail.com>
2017-04-13 15:09:16 +0300

    game/fix: fix pathes

--- dist/bundle.js
+++ dist/bundle.js
@@ -3956,7 +3956,7 @@ define(String.prototype, "padRight", "".padEnd);
         const text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' });
         text.anchor.setTo(0.5, 0.5);
 
-        this.load.image('loaderBg', '../assets/images/loader-bg.png');
+        this.load.image('loaderBg', 'assets/images/loader-bg.png');
         this.load.image('loaderBar', '../assets/images/loader-bar.png');
         this.load.image('underwater', '../assets/images/1.png');
         this.load.image('button', '../assets/images/gorillaMain.png');
